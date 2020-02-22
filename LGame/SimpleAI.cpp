91a035fd68530e6b62a7b61fd29386c52c451820
Jack Malone <c00236428@itcarlow.ie>
2020-02-22 13:13:23 +0000

    trying to figure out where the ai should move to

--- LGame/SimpleAI.cpp
+++ LGame/SimpleAI.cpp
@@ -21,6 +21,60 @@ std::array<std::string, 4> SimpleAI::movePiece(const Board&  t_board)
 		}
 		j = 0;
 	}
+
 	newLocations = oldLocations;
+	j = 0;
+	index = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		for (; j < 4; j++)
+		{
+			newLocations = oldLocations;
+			if (t_board.getCharacter(i, j) == '-' ||
+				t_board.getCharacter(i,j) == '2')
+			{
+				newLocations.at(0) = std::to_string(i + 1) + std::to_string(j + 1);
+				if (i - 1 > 0)
+				{
+					if (t_board.getCharacter(i - 1, j) == '-' ||
+						t_board.getCharacter(i - 1, j) == '2')
+					{
+						newLocations.at(1) = std::to_string(i) + std::to_string(j + 1);
+						if (i - 2 > 0)
+						{
+							if (t_board.getCharacter(i - 2, j) == '-' ||
+								t_board.getCharacter(i - 2, j) == '2')
+							{
+								newLocations.at(2) = std::to_string(i - 2) + std::to_string(j + 1);
+								if (j - 1 > 0)
+								{
+									if (t_board.getCharacter(i - 2, j - 1) == '-'
+										||
+										t_board.getCharacter(i - 2, j - 2) == '2')
+									{
+										newLocations.at(3) = std::to_string(i - 2) + std::to_string(j);
+										return newLocations;
+									}
+								
+								}
+								else if (j + 1 < 4)
+								{
+									if (t_board.getCharacter(i - 2, j + 1) == '-' ||
+										t_board.getCharacter(i - 2, j + 1) == '2')
+									{
+										newLocations.at(3) = std::to_string(i - 2) + std::to_string(j + 2);
+										return newLocations;
+									}									
+								}
+							}							
+							newLocations = oldLocations;
+					    }					
+					}
+				}
+			}
+		}
+		newLocations = oldLocations;
+	}
+	
 	return newLocations;
 }
