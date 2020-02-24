c46be35874f00a4359319668711bde67bd38ec9a
MichalKrzyzanowski <c00240696@itcarlow.ie>
2020-02-24 20:12:31 +0000

    added turn order functionality

--- LGame/Game.cpp
+++ LGame/Game.cpp
@@ -11,7 +11,9 @@ void Game::run()
 			break;
 		}
 
+		
 		Renderer::drawBoard(std::cout, m_board);
+		std::cout << m_turn << std::endl << std::endl;
 
 		update();
 	}
@@ -22,11 +24,39 @@ void Game::update()
 	// update board with information
 	int toQuit{ 0 };
 
+	// allow player to move L-Piece and a neutral piece
+	if (m_currentTurn == TurnOrder::PLAYER_TURN)
+	{
+		switchTurn();
+	}
+
+	// A.I does it's move with the L-Piece
+	else if (m_currentTurn == TurnOrder::AI_TURN)
+	{
+		switchTurn();
+	}
+
+
 	std::cout << "Quit? 1 - Yes: ";
 	std::cin >> toQuit;
-
+	
 	if (toQuit == 1)
 	{
 		quit = true;
+	}
+}
+
+void Game::switchTurn()
+{
+	if (m_currentTurn == TurnOrder::PLAYER_TURN)
+	{
+		m_currentTurn = TurnOrder::AI_TURN;
+		m_turn = "AI's Turn";
+	}
+
+	else
+	{
+		m_currentTurn = TurnOrder::PLAYER_TURN;
+		m_turn = "Player's Turn";
 	}
 }
--- LGame/Game.h
+++ LGame/Game.h
@@ -5,17 +5,25 @@
 #include "Renderer.h"
 #include "Board.h"
 
+enum class TurnOrder
+{
+	PLAYER_TURN,
+	AI_TURN
+};
+
 class Game
 {
 	// variables
 	bool quit{ false };
 
 	Board m_board;
-
+	TurnOrder m_currentTurn{ TurnOrder::PLAYER_TURN };
+	std::string m_turn{ "Player's Turn" };
 
 public:
 	void run();
 	void update();
+	void switchTurn();
 
 };
 
